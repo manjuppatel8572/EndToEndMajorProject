9b0ddc7b7f66224a306c28f0629d93b050db083b
[manjuppatel8572@gmail.com] <manjuppatem8572@gmail.com>
2026-03-23 17:34:43 +0530

    logs,exception and util modules are added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -46,15 +46,14 @@ def create_directories(path_to_directories:list,verbose=True):
 def save_json(path:Path,data:dict):
      """save json data
 
-    Args:
+     Args:
         path (Path): path to json file
         data (dict): data to be saved in json file
-    """
-    with open(path, "w") as f:
-        json.dump(data, f, indent=4)
+     """
+     with open(path,'w') as f:
+         json.dump(data,f, indent=4)
 
-    logger.info(f"json file saved at: {path}")
-
+     logger.info(f"json file saved at: {path}")
 
 
 
