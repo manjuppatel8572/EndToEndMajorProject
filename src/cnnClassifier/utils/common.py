4303680e9f55d6628aaa9a55dadabf843cfb7918
[manjuppatel8572@gmail.com] <manjuppatem8572@gmail.com>
2026-03-25 02:36:00 +0530

    Data Ingestion completed and added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -40,7 +40,12 @@ def create_directories(path_to_directories:list,verbose=True):
     
     Args:
         path_to_directories(list): list of path of directories
-        ignore_log(bool, optional): ignore if multiple dirs is to be created ."""
+        ignore_log(bool, optional): ignore if multiple dirs is to be created .
+        """
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at:{path}")
 
 @ensure_annotations
 def save_json(path:Path,data:dict):
